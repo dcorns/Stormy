bc2d27c70b452d61c3909db8a644db223ee35e24
Dale <drcorns@hotmail.com>
2016-02-22 15:19:28 -0800

    Create custom adapter DayAdapter

---
+++ app/src/main/java/com/dalecorns/stormy/adapters/DayAdapter.java
@@ -0,0 +1,62 @@
+package com.dalecorns.stormy.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dalecorns.stormy.R;
+import com.dalecorns.stormy.weather.Daily;
+
+/**
+ * Created by dcorns on 2/22/16.
+ */
+public class DayAdapter extends BaseAdapter{
+    private Context mContext;
+    Daily[] mDays;
+    public DayAdapter(Context context, Daily[] days){
+        mContext = context;
+        mDays = days;
+    }
+    @Override
+    public int getCount() {
+        return mDays.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mDays[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;//not using
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = new ViewHolder();
+        if(convertView == null){
+            //new View
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.dayLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+            holder.temperatureLabel = (TextView) convertView.findViewById(R.id.temperatureLabel);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        return null;
+    }
+
+    private static class ViewHolder{
+        ImageView iconImageView;
+        TextView temperatureLabel;
+        TextView dayLabel;
+    }
+}
